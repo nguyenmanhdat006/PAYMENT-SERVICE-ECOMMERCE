ae9e4957035463c7f5a40b43142d8e62f9df1d65
nguyendat <manhdatvbhp@gmail.com>
2026-05-20 13:41:10 +0700

    fix: payment < 1000

--- src/main/java/com/ecommerce/paymentservice/dto/request/CreatePaymentRequest.java
+++ src/main/java/com/ecommerce/paymentservice/dto/request/CreatePaymentRequest.java
@@ -26,7 +26,7 @@ public class CreatePaymentRequest {
     private String userId;
 
     @NotNull(message = "Amount is required")
-    @DecimalMin(value = "1000", message = "Amount must be at least 1000 VND")
+    @DecimalMin(value = "1", message = "Amount must be at least 1 USD")
     private BigDecimal amount;
 
     @NotNull(message = "Payment method is required")
@@ -34,4 +34,3 @@ public class CreatePaymentRequest {
 
     private String description;
 }
-
